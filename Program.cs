cc077520869c584c1e198b9a97b61df64dd96d8d
a-tibi <ahmd_tibi@yahoo.com>
2023-05-10 19:52:09 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DailyTask_Enum_Flags
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var day = (DAY.SATURDAY | DAY.FRIDAY);
+			if(day.HasFlag(DAY.WEEKEND))
+			{
+				Console.WriteLine("enjoy your weekend!");
+			}
+			Console.ReadKey();
+		}
+	}
+
+	[Flags]
+	enum DAY
+	{
+		NONE		= 0b0000_0000,
+		FRIDAY		= 0b0000_0001,
+		SATURDAY	= 0b0000_0010,
+		SUNDAY		= 0b0000_0100,
+		MONDAY		= 0b0000_1000,
+		TUESDAY		= 0b0001_0000,
+		WEDENSDAY	= 0b0010_0000,
+		THURSDAY	= 0b0100_0000,
+		WEEKEND     = FRIDAY | SATURDAY,
+		BUSSINESS	= SUNDAY | MONDAY | THURSDAY | WEDENSDAY | THURSDAY
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"# DailyTask_Enum_Flags" 
